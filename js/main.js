c012d065aeebc45b3611afeff4897676664fa36e
Gleb Limanskiy <33000609+UnicalN@users.noreply.github.com>
2022-04-08 12:09:30 +0300

    Reverse

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 import {validateOffer, setUserFormSubmit} from './modules/interaction-with-form.js';
 import {getData} from './modules/server-interfaces.js';
-import { resetMap, map, updatePins} from './modules/map.js';
+import { resetMap} from './modules/map.js';
 import {enablePage, disablePage} from './modules/enable-disable-page.js';
 disablePage();
-map.whenReady(() => {
-  enablePage();
-  validateOffer();
-  getData(updatePins);
-  resetMap();
-  setUserFormSubmit();
-});
+enablePage();
+validateOffer();
+getData();
+resetMap();
+setUserFormSubmit();
